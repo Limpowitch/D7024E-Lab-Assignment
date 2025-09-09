48328a7b670b8675a568a0ab9a4fd334d1e2ad9a
Samuel Thelin <samuel.a.thelin@gmail.com>
2025-09-09 13:47:01 +0200

    created directory for UDP server/client. started extracting needed logic from sprint 0 demo

---
+++ kademlia/internal/transport/udp.go
@@ -0,0 +1,39 @@
+package main
+
+import "net"
+
+type Handler func(from *net.UDPAddr)
+
+type UDPServer struct {
+	pc            net.PacketConn
+	addressString string
+	handler       Handler
+	down          chan struct{}
+}
+
+func (server *UDPServer) Addr() string { return server.addressString }
+
+// needs envelope?
+func (server *UDPServer) Start() {}
+
+// subject to change depending on the payload envelope (needed?) //samme
+func (server *UDPServer) Send(target string, payload string) error {
+	conn, err := net.Dial("udp", target) // open udp socket
+	if err != nil {
+		return err
+	}
+	defer conn.Close()                   // always defer before action to ensure we release socket (straight from tutorial)
+	_, err = conn.Write([]byte(payload)) // send msg
+	return err
+}
+
+func (server *UDPServer) Reply(target *net.UDPAddr, payload string) error {
+	_, err := server.pc.WriteTo(payload, target) // change payload to envelope once struct and functinos are implemented
+	return err
+}
+
+func (server *UDPServer) Close() error {
+	close(server.down)
+
+	return server.pc.Close()
+}
