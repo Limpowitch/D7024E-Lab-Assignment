7452b571f60dd69663e1715b83a86726b977fb06
Samuel Thelin <samuel.a.thelin@gmail.com>
2025-09-15 15:20:30 +0200

    added marshan and unmarshal in order for envelope logic to function

--- kademlia/wire/wire.go
+++ kademlia/wire/wire.go
@@ -1,5 +1,7 @@
 package wire
 
+import "errors"
+
 // we need messages that travel over udp.
 // each message must carry the rpc id (160bit) so that req. and reply can be correlated
 // need to know message type
@@ -21,4 +23,33 @@ type Envelope struct {
 	ID      RPCID
 	Type    string
 	Payload []byte
+}
+
+// Marshal: [20B ID][1B typLen][typ][payload]
+func (e Envelope) Marshal() []byte {
+	typ := []byte(e.Type)
+	if len(typ) > 255 {
+		typ = typ[:255]
+	}
+	out := make([]byte, 0, SizeOfID+1+len(typ)+len(e.Payload))
+	out = append(out, e.ID[:]...)
+	out = append(out, byte(len(typ)))
+	out = append(out, typ...)
+	out = append(out, e.Payload...)
+	return out
+}
+
+func Unmarshal(b []byte) (Envelope, error) {
+	if len(b) < SizeOfID+1 {
+		return Envelope{}, errors.New("short")
+	}
+	var id RPCID
+	copy(id[:], b[:SizeOfID])
+	typLen := int(b[SizeOfID])
+	if len(b) < SizeOfID+1+typLen {
+		return Envelope{}, errors.New("short type")
+	}
+	typ := string(b[SizeOfID+1 : SizeOfID+1+typLen])
+	pl := b[SizeOfID+1+typLen:]
+	return Envelope{ID: id, Type: typ, Payload: pl}, nil
 }
