05456fadce6afc8bec10de7b5546058c47172549
samlthelin <samuel.a.thelin@gmail.com>
2025-09-17 21:02:05 +0200

    moved helper function from routingTable and added xor and closest logic

---
+++ kademlia/cmd/node/routingTable_closest.go
@@ -0,0 +1,47 @@
+package node
+
+import "sort"
+
+func xor(a, b [20]byte) (out [20]byte) {
+	for i := 0; i < 20; i++ {
+		out[i] = a[i] ^ b[i]
+	}
+	return
+}
+func less160(a, b [20]byte) bool { // moved it here because it made more sense in my view
+	for i := 0; i < 20; i++ {
+		if a[i] != b[i] {
+			return a[i] < b[i]
+		}
+	}
+	return false
+}
+
+func (rt *RoutingTable) Closest(target [20]byte, k int) []Contact {
+	rt.mu.RLock()
+	defer rt.mu.RUnlock()
+
+	type pair struct {
+		c Contact
+		d [20]byte
+	}
+	tmp := make([]pair, 0, 64)
+
+	for _, b := range rt.BucketList {
+		b.mu.RLock()
+		for _, c := range b.Contacts {
+			tmp = append(tmp, pair{c: c, d: xor(target, c.ID)})
+		}
+		b.mu.RUnlock()
+	}
+
+	sort.Slice(tmp, func(i, j int) bool { return less160(tmp[i].d, tmp[j].d) })
+	if k > len(tmp) {
+		k = len(tmp)
+	}
+	out := make([]Contact, k)
+	for i := 0; i < k; i++ {
+		out[i] = tmp[i].c
+	}
+	return out
+}
